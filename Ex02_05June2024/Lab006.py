40fb90e65be9d4791f65da295bbef05f17290acb
abhishu0116 <abhishunandwani1993@gmail.com>
2024-06-23 18:13:56 +0530

    Adding Lab006 file

--- Ex02_05June2024/Lab006.py
+++ Ex02_05June2024/Lab006.py
@@ -3,5 +3,5 @@
 print(max(7,8))
 #we can use multiple numbers max(*arg)
 print(max(4,8,-4,9,102,0.5))
-#string should not use here (Type error)
-#print(max(4,6,-4,8,100,0.5,'Abhishu'))
+#string should not used here (Type error)
+#print(max(4,8,-4,9,102,0.5,'Abhishu'))
