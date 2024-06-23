516dad2171013a655f8dba832ddc7ff67b2c9251
abhishu0116 <abhishunandwani1993@gmail.com>
2024-06-23 18:06:43 +0530

    Adding Lab005 file

--- Ex02_05June2024/Lab005.py
+++ Ex02_05June2024/Lab005.py
@@ -1,4 +1,4 @@
-#print("Hello", "Saurabh", 2+2, True, 3*5)
+#print("Hello", "Abhishu", 2+2, True, 3*5)
 print("Hi, My name is Abhishu", "Nandwani")
 print("Hi", "Abhishu", "Nandwani",sep="-", end="")
 print("Hi", "Abhishu", "Nandwani", end="\t")
