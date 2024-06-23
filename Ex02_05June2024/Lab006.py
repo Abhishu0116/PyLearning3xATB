3bab1f035b79825c35a2118e396a376dddd8ce1b
abhishu0116 <abhishunandwani1993@gmail.com>
2024-06-23 18:11:49 +0530

    Adding Lab006 file

---
+++ Ex02_05June2024/Lab006.py
@@ -0,0 +1,7 @@
+# Program to find the max between 2 numbers
+#program to find maximum of 2 numbers
+print(max(7,8))
+#we can use multiple numbers max(*arg)
+print(max(4,8,-4,9,102,0.5))
+#string should not use here (Type error)
+#print(max(4,6,-4,8,100,0.5,'Abhishu'))
