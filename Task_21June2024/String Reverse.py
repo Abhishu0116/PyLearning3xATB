4e28eb8a210de05dd78cd3347b2c4268d24cdc0d
abhishu0116 <abhishunandwani1993@gmail.com>
2024-06-25 22:55:04 +0530

    Adding String Reverse file

---
+++ Task_21June2024/String Reverse.py
@@ -0,0 +1,26 @@
+#Different Ways to reverse a string:
+
+print("****************************************************************************************************************")
+
+new_string="My Name is Abhishu"
+rev_string="";
+
+
+# print(new_string)
+
+for i in range(len(new_string)-1,-1,-1):
+    rev_string+=new_string[i]
+
+print(rev_string);
+
+#Second Way
+print(new_string[::-1])
+
+#Third Way
+
+rev_list=[];
+for char in new_string:
+    rev_list.append(char);
+
+rev_list.reverse()
+print(rev_list)
